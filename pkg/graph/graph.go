f4d6c9e36028eb657fd9f0b83da506183563c813
Adrienne Rio Wongso Atmojo <aatm0001@student.monash.edu>
2025-01-23 22:25:17 +0800

    chore: base types

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,9 +1,26 @@
 package graph
 
-type Package struct {
+type Train struct {
+	Name            string
+	StartingStation Station
 }
 
-type Node struct {
+type Package struct {
+	Name            string
+	Weight          int
+	StartingStation Station
+	EndingStation   Station
+}
+
+type Station struct {
+	Name   string
+	Routes []Route
+}
+
+type Route struct {
+	Name       string
+	TravelTime int
+	Stations   [2]Station
 }
 
 type Graph struct{}
